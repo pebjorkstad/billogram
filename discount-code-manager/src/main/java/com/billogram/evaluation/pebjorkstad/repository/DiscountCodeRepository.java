c2bb23ac4f0d88f1d3915e27dec3adbfa7d78d34
Per-Erik Björkstad <per-erik.bjorkstad@assabloy.com>
2021-12-13 14:10:00 +0100

    Create table if it doesn't exist

--- discount-code-manager/src/main/java/com/billogram/evaluation/pebjorkstad/repository/DiscountCodeRepository.java
+++ discount-code-manager/src/main/java/com/billogram/evaluation/pebjorkstad/repository/DiscountCodeRepository.java
@@ -1,21 +1,33 @@
 package com.billogram.evaluation.pebjorkstad.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.services.dynamodb.model.ResourceInUseException;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.UUID;
 
+@Slf4j
 @Repository
 public class DiscountCodeRepository {
 
     private final DynamoDbEnhancedClient dbClient = DynamoDbEnhancedClient.create();
     private final DynamoDbTable<DiscountCode> table =
             dbClient.table("DiscountCode", TableSchema.fromBean(DiscountCode.class));
+
+    public DiscountCodeRepository() {
+        try {
+            table.createTable();
+            log.info("'DiscountCode' created");
+        } catch (ResourceInUseException e) {
+            log.info("'DiscountCode' table already exists, skipping creation of table");
+        }
+    }
 
     public void saveCodes(UUID brandId, List<String> newDiscountCodes) {
         newDiscountCodes.forEach(code ->
