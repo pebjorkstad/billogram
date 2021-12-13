03ce9cdbb0f51d915c0e6a04dfaccf8509447dce
Per-Erik Björkstad <per-erik.bjorkstad@assabloy.com>
2021-12-13 12:55:58 +0100

    Add documentation for the endpoints

--- discount-code-manager/src/main/java/com/billogram/evaluation/pebjorkstad/controller/DiscountCodeController.java
+++ discount-code-manager/src/main/java/com/billogram/evaluation/pebjorkstad/controller/DiscountCodeController.java
@@ -15,6 +15,12 @@ public class DiscountCodeController {
 
     private final DiscountCodeHandler handler = new DiscountCodeHandler();
 
+    /**
+     * Allows a brand to create a given amount of discount codes for its brand
+     * @param request {@link CreateCodesRequest}
+     * @return HTTP 200 OK If the amount of discount codes were generated successfully.
+     *                     The generated discount codes will also be returned in the response body.
+     */
     @PostMapping
     public CreateCodesResponse post(@RequestBody CreateCodesRequest request) {
         List<String> codes = handler.generateCodes(request.getBrandId(), request.getNrOfCodes());
@@ -23,6 +29,14 @@ public class DiscountCodeController {
                 .build();
     }
 
+    /**
+     * Used by a user to request a discount code from a brand. If a discount code exists for the brand,
+     * a discount code will be assigned to the user and returned in the body of a Http 200 Ok response.
+     * @param request {@link GetDiscountCodeRequest}
+     * @return HTTP 200 OK        If there was an unused discount code from the brand and it was
+     *                            successfully assigned to the user
+     *         HTTP 404 NOT FOUND If the brand did not have any free discount codes left
+     */
     @GetMapping
     public GetDiscountCodeResponse getCode(@RequestBody GetDiscountCodeRequest request) {
         DiscountCode discountCode = handler.assignCode(request.getBrandId(), request.getUserId());
